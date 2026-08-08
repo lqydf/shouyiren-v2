9561213bae123a7a95af01a8ee84df0ebc437c35
lqydf <lqydf123@gmail.com>
2026-08-09 05:22:02 +0800

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-shell">
+    <Navbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+</script>
