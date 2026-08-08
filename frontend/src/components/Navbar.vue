47807f57d1bf483ca4e3eeda41546ebda7480967
lqydf <lqydf123@gmail.com>
2026-08-09 05:23:17 +0800

    Create Navbar.vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="navbar">
+    <RouterLink class="brand" to="/" aria-label="返回守艺人首页">
+      <span class="brand-mark">守</span>
+      <span class="brand-text">守艺人</span>
+    </RouterLink>
+
+    <nav class="nav-links" aria-label="主导航">
+      <RouterLink to="/">首页</RouterLink>
+      <RouterLink to="/glazes">釉方大全</RouterLink>
+      <RouterLink class="login-link" to="/login">登录</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
