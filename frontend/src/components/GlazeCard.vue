1caaa6f5e261e2ab8b3657f301515c3f411bd82b
lqydf <lqydf123@gmail.com>
2026-08-09 05:24:04 +0800

    Add GlazeCard component for displaying glaze details

---
+++ frontend/src/components/GlazeCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="glaze-card">
+    <div class="glaze-card__swatch" :style="{ background: glaze.color }" />
+    <div class="glaze-card__content">
+      <p class="glaze-card__kiln">{{ glaze.kiln }}</p>
+      <h3>{{ glaze.name }}</h3>
+      <p>{{ glaze.description }}</p>
+      <dl>
+        <div>
+          <dt>烧成</dt>
+          <dd>{{ glaze.temperature }}</dd>
+        </div>
+        <div>
+          <dt>气氛</dt>
+          <dd>{{ glaze.atmosphere }}</dd>
+        </div>
+      </dl>
+    </div>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  glaze: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
